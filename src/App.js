4ce4bb542b5e756ffffa5363f2d44a421bfb2207
Stokeley999999 <yourstrulystokeley@gmail.com>
2024-05-09 12:58:00 +0300

    created navbar links with Home,Contact and About and a navbar functional component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+
+<h2> Navbar </h2>
+    <Navbar/>
+    </div>
+  );
+}
+
+export default App;
